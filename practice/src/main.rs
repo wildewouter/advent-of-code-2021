99693918e20ca3c243ab7fdb38a2f3e48713ce14
Wouter de Wild <wouter@codewild.nl>
2021-11-25 14:19:19 +0100

    Practice day 1 part one 2020

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,3 +1,27 @@
+mod read;
+
 fn main() {
-    println!("Hello, world!");
+    let content = read::file("input");
+
+    let lines: Vec<String> = content.lines().map(|x| String::from(x)).collect();
+
+    let result = lines
+        .iter()
+        .filter_map(|line| compare(line, &lines))
+        .fold(1, |result, x| result * x);
+
+    println!("{:?}", result);
+}
+
+fn compare(line: &str, other_lines: &[String]) -> Option<usize> {
+    let number = line.parse::<usize>().ok()?;
+
+    for x in other_lines {
+        let i = x.parse::<usize>().ok()?;
+        if i + number == 2020 {
+            return Some(i);
+        }
+    }
+
+    None
 }

9f5682ec64a9398557e4732f5a7e58a038ed1f1a
Wouter de Wild <wouter@codewild.nl>
2021-11-25 20:16:58 +0100

    Added day one part two

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -5,12 +5,30 @@ fn main() {
 
     let lines: Vec<String> = content.lines().map(|x| String::from(x)).collect();
 
-    let result = lines
+    let day_one = day_one(&lines);
+    let day_two = day_two(&lines);
+
+    println!("Day one: {}", day_one);
+    println!("Day two: {}", day_two);
+}
+
+fn day_one(lines: &Vec<String>) -> usize {
+    lines
         .iter()
         .filter_map(|line| compare(line, &lines))
-        .fold(1, |result, x| result * x);
+        .fold(1, |result, x| result * x)
+}
 
-    println!("{:?}", result);
+fn day_two(lines: &Vec<String>) -> usize {
+    let number_lines: Vec<usize> = lines
+        .iter()
+        .filter_map(|line| line.parse::<usize>().ok())
+        .collect();
+
+    number_lines
+        .iter()
+        .filter_map(|line| compare_day_two(line, &number_lines))
+        .fold(1, |result, x| result * x)
 }
 
 fn compare(line: &str, other_lines: &[String]) -> Option<usize> {
@@ -25,3 +43,16 @@ fn compare(line: &str, other_lines: &[String]) -> Option<usize> {
 
     None
 }
+
+fn compare_day_two(line: &usize, other_lines: &[usize]) -> Option<usize> {
+    for line_2 in other_lines {
+        for line_3 in other_lines {
+            if (line + line_2 + line_3) == 2020 {
+                return Some(line.clone());
+            }
+        }
+    }
+
+    None
+}
+

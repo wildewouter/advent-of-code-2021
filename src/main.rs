85017d8a839497221d03f2f7855b0634b6e35984
Wouter de Wild <wouter@codewild.nl>
2021-12-01 09:38:37 +0100

    Day one part one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod read;
+
 fn main() {
-    println!("Hello world")
+    let content = read::file("day-1/input");
+
+    let lines: Vec<usize> = content
+        .lines()
+        .filter_map(|line| line.parse::<usize>().ok())
+        .collect();
+
+    println!("Part one: {}", part_one(&lines));
+}
+
+fn part_one(lines: &Vec<usize>) -> usize {
+    lines
+        .iter()
+        .zip(lines.iter().skip(1))
+        .fold(0, |total, (previous, next)| {
+            if next > previous {
+                return total + 1;
+            }
+
+            return total;
+        })
 }

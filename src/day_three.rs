bd7aef332795a3d3d3509653a77a244643046639
Wouter de Wild <wouter@codewild.nl>
2021-12-06 11:22:20 +0100

    Day three complete

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,17 +1,18 @@
-use std::borrow::Borrow;
-use std::process::exit;
-use std::str::FromStr;
-use regex::{Match, Regex};
 use crate::read;
+use std::str::FromStr;
 
 pub fn run() {
-    let content: Vec<Binary> = read::file("input/day-3/input-example").lines().filter_map(|line| line.parse::<Binary>().ok()).collect();
+    let content: Vec<Binary> = read::file("input/day-3/input")
+        .lines()
+        .filter_map(|line| line.parse::<Binary>().ok())
+        .collect();
 
     println!("Day three:");
-    // println!("Part one: {}", part_one(&content));
+    println!("Part one: {}", part_one(&content));
     println!("Part two: {}", part_two(&content));
 }
 
+#[derive(Clone, Debug)]
 struct Binary {
     split: Vec<usize>,
 }
@@ -21,7 +22,11 @@ impl FromStr for Binary {
 
     fn from_str(input: &str) -> Result<Binary, ()> {
         Ok(Binary {
-            split: input.split("").into_iter().filter_map(|val| val.parse::<usize>().ok()).collect(),
+            split: input
+                .split("")
+                .into_iter()
+                .filter_map(|val| val.parse::<usize>().ok())
+                .collect(),
         })
     }
 }
@@ -54,33 +59,34 @@ fn part_one(input: &[Binary]) -> usize {
 fn count(input: &[Binary]) -> Vec<usize> {
     let start: Option<Vec<usize>> = None;
 
-    input.iter().fold(start, |total, next| {
-        match total {
+    input
+        .iter()
+        .fold(start, |total, next| match total {
             None => Some(next.split.clone()),
             Some(result) => {
                 let mut counter: Vec<usize> = result.clone();
                 for (index, bit) in next.split.iter().enumerate() {
                     counter[index] = result[index] + bit;
                 }
-
                 Some(counter)
             }
-        }
-    }).unwrap()
+        })
+        .unwrap()
 }
 
 fn get_most_common(input: &[Binary]) -> Binary {
     let count = count(input);
-
     Binary {
         split: count
-            .iter().map(|&bit| {
-            if bit > input.iter().count() / 2 {
-                return 1;
-            }
+            .iter()
+            .map(|&bit| {
+                if bit * 2 >= input.len() {
+                    return 1;
+                }
 
-            0
-        }).collect()
+                0
+            })
+            .collect(),
     }
 }
 
@@ -89,50 +95,90 @@ fn get_least_common(input: &[Binary]) -> Binary {
 
     Binary {
         split: count
-            .iter().map(|&bit| {
-            if bit <= input.iter().count() / 2 {
-                return 1;
-            }
+            .into_iter()
+            .map(|bit| {
+                if bit * 2 < input.len() {
+                    return 1;
+                }
 
-            0
-        }).collect()
+                0
+            })
+            .collect(),
     }
 }
 
 fn part_two(input: &[Binary]) -> usize {
-    let most_common = get_most_common(input);
-    let least_common = get_least_common(input);
+    let oxigen: usize = calc_oxigen(input, None);
+    let co2: usize = calc_co2(input, None);
 
-    let oxigen: Vec<&Binary> = input.iter().filter(|binary| {
-        binary.split.iter().enumerate().fold(true, |result, (index, bit)| {
-            if !result {
-                return result;
-            }
+    co2 * oxigen
+}
 
-            &most_common.split[index] == bit
-        })
-    }).collect();
+fn calc_oxigen(list_to_check: &[Binary], current_index: Option<usize>) -> usize {
+    if list_to_check.len() == 1 {
+        return list_to_check.get(0).unwrap().clone().into();
+    }
 
-    let co2 = least_common.split.iter().enumerate().fold(input, |result, (index, bit)| {
-        if result.len() == 1 {
-            return result;
-        }
+    let most_common = get_most_common(list_to_check);
+    let index = current_index.unwrap_or(0);
+    let bit = most_common.split.get(index).unwrap();
+    let remaining_bits = list_to_check
+        .iter()
+        .cloned()
+        .filter(|binary| binary.split[index] == *bit)
+        .collect::<Vec<Binary>>();
 
-        result.into_iter().filter(|binary| {
-            binary.split[index] == *bit
-        }).collect()
-    });
-    // let co2: Vec<&Binary> = input.iter().filter(|binary| {
-    //     binary.split.iter().enumerate().fold(true, |result, (index, bit)| {
-    //         if !result {
-    //             return result;
-    //         }
-    //
-    //         &least_common.split[index] == bit
-    //     })
-    // }).collect();
+    calc_oxigen(&remaining_bits, Some(index + 1))
+}
 
-    //todo: Get items from array and product
-    dbg!(co2.len());
-    0
+fn calc_co2(list_to_check: &[Binary], current_index: Option<usize>) -> usize {
+    if list_to_check.len() == 1 {
+        return list_to_check.get(0).unwrap().clone().into();
+    }
+
+    let least_common = get_least_common(list_to_check);
+    let index = current_index.unwrap_or(0);
+    let bit = least_common.split.get(index).unwrap();
+    let remaining_bits = list_to_check
+        .iter()
+        .cloned()
+        .filter(|binary| binary.split[index] == *bit)
+        .collect::<Vec<Binary>>();
+
+    calc_co2(&remaining_bits, Some(index + 1))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_three::get_most_common;
+    use crate::day_three::Binary;
+
+    #[test]
+    fn it_should_count_most_common() {
+        let result = get_most_common(&[
+            Binary {
+                split: vec![1, 1, 1, 1, 0],
+            },
+            Binary {
+                split: vec![1, 0, 1, 1, 0],
+            },
+            Binary {
+                split: vec![1, 0, 1, 1, 1],
+            },
+            Binary {
+                split: vec![1, 0, 1, 0, 1],
+            },
+            Binary {
+                split: vec![1, 1, 1, 0, 0],
+            },
+            Binary {
+                split: vec![1, 0, 0, 0, 0],
+            },
+            Binary {
+                split: vec![1, 1, 0, 0, 1],
+            },
+        ]);
+
+        assert_eq!("10100", result.to_string())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-mod read;
+use std::time::Instant;
+
 mod day_one;
-mod day_two;
 mod day_three;
+mod day_two;
+mod read;
 
 fn main() {
-    // day_one::run();
-    // println!("_________________________________");
-    // day_two::run();
-    // println!("_________________________________");
+    day_one::run();
+    println!("_________________________________");
+    day_two::run();
+    println!("_________________________________");
     day_three::run();
     println!("_________________________________");
 }

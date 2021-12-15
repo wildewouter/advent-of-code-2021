e758ec64fed1d061c34d7d6e1b2c0753beb8f311
Wouter de Wild <wouter@codewild.nl>
2021-12-15 17:25:37 +0100

    Day nine part two

--- packages/day9/src/main.rs
+++ packages/day9/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     println!("Day nine");
     println!("Part one: {}", part_one(&grid));
+    println!("Part two: {}", part_two(&grid));
 }
 
 fn part_one(grid: &HashMap<(i32, i32), i32>) -> usize {
@@ -28,4 +29,64 @@ fn part_one(grid: &HashMap<(i32, i32), i32>) -> usize {
     }
 
     low_points.iter().sum()
+}
+
+fn part_two(grid: &HashMap<(i32, i32), i32>) -> usize {
+    let mut checked: Vec<(i32, i32)> = Vec::new();
+    let mut basins: Vec<usize> = Vec::new();
+
+    for (position, value) in grid.iter() {
+        if checked.contains(position) {
+            continue;
+        }
+
+        if value == &9 {
+            checked.push(*position);
+            continue;
+        }
+
+        let basin = check_grid(position, &mut Vec::new(), grid);
+        basins.push(basin.len());
+        checked.extend(&basin);
+    }
+
+    basins.sort();
+    basins.iter().rev().take(3).product::<usize>()
+}
+
+fn check_grid(
+    position: &(i32, i32),
+    basin: &mut Vec<(i32, i32)>,
+    grid: &HashMap<(i32, i32), i32>,
+) -> Vec<(i32, i32)> {
+    if basin.contains(position) {
+        return basin.clone();
+    }
+
+    let value = grid.get(position).unwrap();
+
+    if value == &9 {
+        return basin.clone();
+    }
+
+    basin.push(*position);
+
+    let mut neighbours_in_basin: Vec<(i32, i32)> = Vec::new();
+
+    let (x, y) = *position;
+    for coordinate in vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)] {
+        if let Some(neighbour) = grid.get(&coordinate) {
+            if neighbour == &9 {
+                continue;
+            }
+
+            neighbours_in_basin.push(coordinate);
+        }
+    }
+
+    for pos in neighbours_in_basin {
+        check_grid(&pos, basin, grid);
+    }
+
+    basin.clone()
 }

c6570c484d3da3e3d3c57925ff1dce09f78ed25e
Wouter de Wild <wouter@codewild.nl>
2021-12-19 23:25:53 +0100

    Optimizations

--- packages/day14/src/main.rs
+++ packages/day14/src/main.rs
@@ -28,18 +28,17 @@ fn do_it(start_set: &Vec<&str>, rule_set: &HashMap<String, String>, times: usize
     for pair in start_set.windows(2) {
         let index = format!("{}{}", pair[0], pair[1]);
         let in_set = pairs_in_line.get(&index).unwrap_or(&0).clone();
-        pairs_in_line.insert(index.clone(), in_set + &1);
+        pairs_in_line.insert(index, in_set + &1);
     }
 
     for _ in 0..times {
         let mut pairs_in_next_line: HashMap<String, usize> = HashMap::new();
 
-        for (pair, amount) in pairs_in_line.clone() {
+        for (pair, amount) in pairs_in_line {
             if let Some(letter) = rule_set.get(&pair.to_string()) {
-                let existing: Vec<char> = pair.chars().collect();
                 for combo in vec![
-                    &format!("{}{}", existing[0], letter),
-                    &format!("{}{}", letter, existing[1]),
+                    &format!("{}{}", pair.chars().nth(0).unwrap(), letter),
+                    &format!("{}{}", letter, pair.chars().nth(1).unwrap()),
                 ] {
                     pairs_in_next_line.insert(
                         combo.to_string(),
@@ -55,13 +54,13 @@ fn do_it(start_set: &Vec<&str>, rule_set: &HashMap<String, String>, times: usize
     let mut result_set: HashMap<String, usize> = HashMap::new();
     result_set.insert(start_set[0].to_string(), 1);
 
-    for (pair, value) in pairs_in_line {
-        let letters: Vec<char> = pair.chars().collect();
-
-        let string = letters[1].to_string();
+    for (pair, occurrences) in pairs_in_line {
         result_set.insert(
-            string.clone(),
-            result_set.get(&string).unwrap_or(&0) + value,
+            pair.chars().nth(1).unwrap().to_string(),
+            result_set
+                .get(&pair.chars().nth(1).unwrap().to_string())
+                .unwrap_or(&0)
+                + occurrences,
         );
     }
 

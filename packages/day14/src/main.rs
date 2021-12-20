2571db5c038cdf90f2072c65be85445292b5f958
Wouter de Wild <wouter@codewild.nl>
2021-12-20 10:46:21 +0100

    Very large amount of iterating can be done

--- packages/day14/src/main.rs
+++ packages/day14/src/main.rs
@@ -19,39 +19,42 @@ fn main() {
 
     println!("Day fourteen");
     println!("Part one: {}", do_it(&start_set, &rule_set, 10));
-    println!("Part two: {}", do_it(&start_set, &rule_set, 40));
+    println!("Part two: {}", do_it(&start_set, &rule_set, 126));
 }
 
-fn do_it(start_set: &Vec<&str>, rule_set: &HashMap<String, String>, times: usize) -> usize {
-    let mut pairs_in_line: HashMap<String, usize> = HashMap::new();
+fn do_it(start_set: &Vec<&str>, rule_set: &HashMap<String, String>, times: usize) -> u128 {
+    let mut pairs_in_line: HashMap<String, u128> = HashMap::new();
 
     for pair in start_set.windows(2) {
         let index = format!("{}{}", pair[0], pair[1]);
-        let in_set = pairs_in_line.get(&index).unwrap_or(&0).clone();
-        pairs_in_line.insert(index, in_set + &1);
+        let start_value = pairs_in_line.get(&index).unwrap_or(&0) + 1;
+
+        pairs_in_line.insert(index, start_value);
     }
 
     for _ in 0..times {
-        let mut pairs_in_next_line: HashMap<String, usize> = HashMap::new();
+        let mut pairs_in_next_line: HashMap<String, u128> = HashMap::new();
 
-        for (pair, amount) in pairs_in_line {
+        pairs_in_line.iter().for_each(|(pair, amount)| {
             if let Some(letter) = rule_set.get(&pair.to_string()) {
-                for combo in vec![
-                    &format!("{}{}", pair.chars().nth(0).unwrap(), letter),
+                [
+                    &format!("{}{}", pair.chars().next().unwrap(), letter),
                     &format!("{}{}", letter, pair.chars().nth(1).unwrap()),
-                ] {
+                ]
+                .iter()
+                .for_each(|&combo| {
                     pairs_in_next_line.insert(
                         combo.to_string(),
                         pairs_in_next_line.get(combo).unwrap_or(&0) + amount,
                     );
-                }
+                });
             }
-        }
+        });
 
         pairs_in_line = pairs_in_next_line;
     }
 
-    let mut result_set: HashMap<String, usize> = HashMap::new();
+    let mut result_set: HashMap<String, u128> = HashMap::new();
     result_set.insert(start_set[0].to_string(), 1);
 
     for (pair, occurrences) in pairs_in_line {
